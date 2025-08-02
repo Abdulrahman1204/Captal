5d571fadb184e16c835c7febcb034b533124b9d2
Abdulrahman <137169908+Abdulrahman1204@users.noreply.github.com>
2025-08-02 21:51:10 +0300

    Update server.ts

--- server.ts
+++ server.ts
@@ -94,6 +94,6 @@ app.use(errorHandler);
 
 // Start HTTPS server
 const PORT: number = parseInt(process.env.PORT || "8000");
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
